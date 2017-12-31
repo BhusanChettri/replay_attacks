30e522de3971d034cd26d511e4fb810a553c5ffc
bhusanchettri <bhusan.chettri@gmail.com>
2017-12-31 15:26:52 +0000

    changed google arch definition

--- final_code/nn_architecture.py
+++ final_code/nn_architecture.py
@@ -155,8 +155,6 @@ def cnnModel1(input_type,trainSize,input_placeholder,activation,init,targets,fft
 
 def cnnModel2(input_type,trainSize,input_placeholder,activation,init,targets,fftSize,padding,keep_prob1, keep_prob2, keep_prob3):
     
-    # WE use google text dependent DNN setup with 10X10 filter in Conv layer.    
-    
     t=trainSize
     
     trainSize=str(trainSize)+'sec'
@@ -181,50 +179,56 @@ def cnnModel2(input_type,trainSize,input_placeholder,activation,init,targets,fft
             f = 80      # when two types of features are concatenated (eg CQCC+SCMC)
         else:
             f = 40      # just the delta+acceleration (40 dimensional)    
-            
-    # We just use f=10 and t=10, 10X10 convolution
-    f=10
                         
     weight_list = list()
     activation_list = list()
     bias_list = list()
     
     if activation=='mfm':
-        fc_input= f*64   #6448 #1*257*64 = 16448
-        in_conv2 = 64
-        in_conv3 = 64
-        in_conv4 = 64
+        fc_input= f*8   #6448 #1*257*64 = 16448
+        in_conv2 = 8
+        in_conv3 = 8
+        in_conv4 = 8
         in_fc2 = 128
         in_fc3 = 128
         in_outputLayer = 128
         
     else:
-        fc_input= f*128  #32896 # 1*257*128
-        in_conv2 = 128
-        in_conv3 = 128
-        in_conv4 = 128
+        fc_input= f*16  #32896 # 1*257*128
+        in_conv2 = 16
+        in_conv3 = 16
+        in_conv4 = 16
         in_fc2 = 256
         in_fc3 = 256
         in_outputLayer = 256
     print('======================== CNN ARCHITECTURE ==============================\n')    
-                                      
-    
-    conv1,w1,b1 = conv_layer(input_placeholder, [10,10,1,32], [32], [1,1,1,1],'conv1',padding,activation,init)
+                               
+    #Convolution layer1,2,3    
+    conv1,w1,b1 = conv_layer(input_placeholder, [3,10,1,16], [16], [1,1,1,1],'conv1',padding,activation,init)
     weight_list.append(w1)
-    bias_list.append(b1)
+    bias_list.append(b1)    
     print('Conv1 ', conv1)
-            
+    pool1 = maxPool2x2(conv1, [1,2,2,1], [1,2,2,1])
+    
+    conv2,w2,b2 = conv_layer(pool1, [3,10,in_conv2,16], [16], [1,1,1,1],'conv2', padding,activation,init)
+    weight_list.append(w2)
+    bias_list.append(b2)    
+    print('Conv2 ', conv2)
+    pool2 = maxPool2x2(conv2, [1,2,2,1], [1,2,2,1])
+    
+    conv3,w3,b3 = conv_layer(pool2, [3,10,in_conv3,16], [16], [1,1,1,1],'conv3', padding,activation,init)
+    weight_list.append(w3)
+    bias_list.append(b3)    
+    print('Conv3 ', conv3)
+    pool3 = maxPool2x2(conv3, [1,2,2,1], [1,2,2,1])
+    
     if input_type == 'cqt_spec':
         time_dim = 32
     else:
         time_dim = t*100    
-    
-    #Max-pooling layer over time    
-    pool1 = maxPool2x2(conv1, [1,time_dim,1,1], [1,time_dim,1,1])
-    print('Pool1 layer shape = ', pool1)
         
     # Dropout on the huge input from Conv layer    
-    flattened = tf.reshape(pool1, shape=[-1,fc_input])
+    flattened = tf.reshape(pool3, shape=[-1,fc_input])
     dropped_1 = drop_layer(flattened, keep_prob1, 'dropout1')
     
     # Fully connected layer 1 with 256 neurons but gets splitted into 128 due to MFM
@@ -234,15 +238,15 @@ def cnnModel2(input_type,trainSize,input_placeholder,activation,init,targets,fft
     
     print('Shape of FC1 = ', fc1.shape)
     
-    # Dropout followed by FC layer with 256 neurons but gets splitted into 128 due to MFM
-    #dropped_2 = drop_layer(fc1, keep_prob2, 'dropout2')        
-    #fc2,w5,b5, = fc_layer(dropped_2, in_fc2, 256, 'FC_Layer2', activation)
-    #weight_list.append(w5)
-    #bias_list.append(b5)
-    
-    #print('Shape of FC2 = ', fc2.shape)
-
     '''
+    # Dropout followed by FC layer with 256 neurons but gets splitted into 128 due to MFM
+    dropped_2 = drop_layer(fc1, keep_prob2, 'dropout2')        
+    fc2,w5,b5, = fc_layer(dropped_2, in_fc2, 256, 'FC_Layer2', activation)
+    weight_list.append(w5)
+    bias_list.append(b5)
+    
+    print('Shape of FC2 = ', fc2.shape)
+
     # Dropout followed by FC layer with 256 neurons but gets splitted into 128 due to MFM
     dropped_3 = drop_layer(fc2, keep_prob2, 'dropout3')        
     fc3,w6,b6, = fc_layer(dropped_3, in_fc3, 256, 'FC_Layer3', activation)
@@ -250,8 +254,8 @@ def cnnModel2(input_type,trainSize,input_placeholder,activation,init,targets,fft
     bias_list.append(b6)
     
     print('Shape of FC3 = ', fc3.shape)
-    '''
-    
+    ''' 
+
     #Output layer: 2 neurons. One for genuine and one for spoof. Dropout applied first
     dropped_4 = drop_layer(fc1, keep_prob3, 'dropout4')
     output,w7,b7 = fc_layer(dropped_4, in_outputLayer, targets, 'Output_Layer', 'no-activation')  #get raw logits        
@@ -265,4 +269,4 @@ def cnnModel2(input_type,trainSize,input_placeholder,activation,init,targets,fft
     
     return fc1, output, weight_list, activation_list, bias_list
 #---------------------------------------------------------------------------------------------------------------
-
+    
