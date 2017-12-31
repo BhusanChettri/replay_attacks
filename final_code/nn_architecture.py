cb6b1d9857b9fafa5b912f22ad12b18eef75e6bf
bhusanchettri <bhusan.chettri@gmail.com>
2017-12-31 14:17:13 +0000

    added google architecture

--- final_code/nn_architecture.py
+++ final_code/nn_architecture.py
@@ -151,3 +151,118 @@ def cnnModel1(input_type,trainSize,input_placeholder,activation,init,targets,fft
     
     return fc3, output, weight_list, activation_list, bias_list
 #---------------------------------------------------------------------------------------------------------------
+
+
+def cnnModel2(input_type,trainSize,input_placeholder,activation,init,targets,fftSize,padding,keep_prob1, keep_prob2, keep_prob3):
+    
+    # WE use google text dependent DNN setup with 10X10 filter in Conv layer.    
+    
+    t=trainSize
+    
+    trainSize=str(trainSize)+'sec'
+            
+    if input_type=='mel_spec':
+        f= 80
+    elif input_type=='cqt_spec':
+        f = 84        
+    elif input_type=='mag_spec':
+        
+        if fftSize == 512:
+            f = 257
+        elif fftSize == 256:
+            f = 129
+        elif fftSize == 1024:
+            f = 513
+        elif fftSize == 2048: 
+            f = 1025
+    else:
+        concatenate=False
+        if concatenate:
+            f = 80      # when two types of features are concatenated (eg CQCC+SCMC)
+        else:
+            f = 40      # just the delta+acceleration (40 dimensional)    
+            
+    # We just use f=10 and t=10, 10X10 convolution
+    f=10
+                        
+    weight_list = list()
+    activation_list = list()
+    bias_list = list()
+    
+    if activation=='mfm':
+        fc_input= f*64   #6448 #1*257*64 = 16448
+        in_conv2 = 64
+        in_conv3 = 64
+        in_conv4 = 64
+        in_fc2 = 128
+        in_fc3 = 128
+        in_outputLayer = 128
+        
+    else:
+        fc_input= f*128  #32896 # 1*257*128
+        in_conv2 = 128
+        in_conv3 = 128
+        in_conv4 = 128
+        in_fc2 = 256
+        in_fc3 = 256
+        in_outputLayer = 256
+    print('======================== CNN ARCHITECTURE ==============================\n')    
+                                      
+    
+    conv1,w1,b1 = conv_layer(input_placeholder, [10,10,1,32], [32], [1,1,1,1],'conv1',padding,activation,init)
+    weight_list.append(w1)
+    bias_list.append(b1)
+    print('Conv1 ', conv1)
+            
+    if input_type == 'cqt_spec':
+        time_dim = 32
+    else:
+        time_dim = t*100    
+    
+    #Max-pooling layer over time    
+    pool1 = maxPool2x2(conv1, [1,time_dim,1,1], [1,time_dim,1,1])
+    print('Pool1 layer shape = ', pool1)
+        
+    # Dropout on the huge input from Conv layer    
+    flattened = tf.reshape(pool1, shape=[-1,fc_input])
+    dropped_1 = drop_layer(flattened, keep_prob1, 'dropout1')
+    
+    # Fully connected layer 1 with 256 neurons but gets splitted into 128 due to MFM
+    fc1,w4,b4, = fc_layer(dropped_1, fc_input, 256, 'FC_Layer1', activation)
+    weight_list.append(w4)
+    bias_list.append(b4)
+    
+    print('Shape of FC1 = ', fc1.shape)
+    
+    # Dropout followed by FC layer with 256 neurons but gets splitted into 128 due to MFM
+    #dropped_2 = drop_layer(fc1, keep_prob2, 'dropout2')        
+    #fc2,w5,b5, = fc_layer(dropped_2, in_fc2, 256, 'FC_Layer2', activation)
+    #weight_list.append(w5)
+    #bias_list.append(b5)
+    
+    #print('Shape of FC2 = ', fc2.shape)
+
+    '''
+    # Dropout followed by FC layer with 256 neurons but gets splitted into 128 due to MFM
+    dropped_3 = drop_layer(fc2, keep_prob2, 'dropout3')        
+    fc3,w6,b6, = fc_layer(dropped_3, in_fc3, 256, 'FC_Layer3', activation)
+    weight_list.append(w6)
+    bias_list.append(b6)
+    
+    print('Shape of FC3 = ', fc3.shape)
+    '''
+    
+    #Output layer: 2 neurons. One for genuine and one for spoof. Dropout applied first
+    dropped_4 = drop_layer(fc1, keep_prob3, 'dropout4')
+    output,w7,b7 = fc_layer(dropped_4, in_outputLayer, targets, 'Output_Layer', 'no-activation')  #get raw logits        
+        
+    weight_list.append(w7)
+    bias_list.append(b7)            
+                
+    print('Output layer shape = ', output.shape)
+    print('======================== CNN ARCHITECTURE ==============================\n')
+    
+    
+    return fc1, output, weight_list, activation_list, bias_list
+#---------------------------------------------------------------------------------------------------------------
+
